53eff06ec8aaab7cb0fa4010c46cc6d584eb0a02
GabrielMoretti <gabrielmorettisilva@gmail.com>
2021-10-01 13:18:42 -0300

    Try and Catch in Routes

--- src/routes/shopping.routes.ts
+++ src/routes/shopping.routes.ts
@@ -8,27 +8,35 @@ import CreateProductService from '../service/CreateProductService';
 const shoppingRouter = Router();
 
 shoppingRouter.get('/', async (request, response) => {
-  const productsRepository = getRepository(Product);
+  try {
+    const productsRepository = getRepository(Product);
 
-  const products = await productsRepository.find();
+    const products = await productsRepository.find();
 
-  return response.json({ products });
+    return response.json({ products });
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
 });
 
 shoppingRouter.post('/', async (request, response) => {
-  const { title, value, cover, available, sold } = request.body;
+  try {
+    const { title, value, cover, available, sold } = request.body;
 
-  const createProduct = new CreateProductService();
+    const createProduct = new CreateProductService();
 
-  const product = await createProduct.execute({
-    title,
-    value,
-    cover,
-    available,
-    sold,
-  });
+    const product = await createProduct.execute({
+      title,
+      value,
+      cover,
+      available,
+      sold,
+    });
 
-  return response.json(product);
+    return response.json(product);
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
 });
 
 export default shoppingRouter;
