f58f485c2dff9a1fb5495aca62b5c4539c498df0
GabrielMoretti <gabrielmorettisilva@gmail.com>
2021-10-03 16:40:21 -0300

    API Connection

--- E-Clothes-FrontEnd/src/pages/Home/index.tsx
+++ E-Clothes-FrontEnd/src/pages/Home/index.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { AiOutlineShoppingCart, AiOutlineShopping } from 'react-icons/ai';
 import { FiGift } from 'react-icons/fi';
+import api from '../../services/api';
 
 import { Header, Main } from './styles';
 
 const Home: React.FC = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    api.get('/shopping').then(response => {
+      setProducts([...products, response.data]);
+    });
+  }, [products]);
+
   return (
     <>
       <Header>
@@ -18,134 +27,24 @@ const Home: React.FC = () => {
         </div>
       </Header>
       <Main>
-        <div className="card">
-          <img src="https://via.placeholder.com/300x200" alt="cover" />
-          <h6>Product Name</h6>
-          <p>R$ 15,00</p>
-          <div id="quantity">
-            <span>Sold: 20</span>
-            <span>Available: 5</span>
-          </div>
-          <button type="button">
-            <div className="icon">
-              <AiOutlineShopping size={19} />
-              <span>3</span>
+        {products.map(product => (
+          <div key={product.id} className="card">
+            <img src="https://via.placeholder.com/300x200" alt="cover" />
+            <h6>Product Name</h6>
+            <p>R$ 15,00</p>
+            <div id="quantity">
+              <span>Sold: 20</span>
+              <span>Available: 5</span>
             </div>
-            <p>ADICIONAR AO CARRINHO</p>
-          </button>
-        </div>
-        <div className="card">
-          <img src="https://via.placeholder.com/300x200" alt="cover" />
-          <h6>Product Name</h6>
-          <p>R$ 15,00</p>
-          <div id="quantity">
-            <span>Sold: 20</span>
-            <span>Available: 5</span>
+            <button type="button">
+              <div className="icon">
+                <AiOutlineShopping size={19} />
+                <span>3</span>
+              </div>
+              <p>ADICIONAR AO CARRINHO</p>
+            </button>
           </div>
-          <button type="button">
-            <div className="icon">
-              <AiOutlineShopping size={19} />
-              <span>3</span>
-            </div>
-            <p>ADICIONAR AO CARRINHO</p>
-          </button>
-        </div>
-        <div className="card">
-          <img src="https://via.placeholder.com/300x200" alt="cover" />
-          <h6>Product Name</h6>
-          <p>R$ 15,00</p>
-          <div id="quantity">
-            <span>Sold: 20</span>
-            <span>Available: 5</span>
-          </div>
-          <button type="button">
-            <div className="icon">
-              <AiOutlineShopping size={19} />
-              <span>3</span>
-            </div>
-            <p>ADICIONAR AO CARRINHO</p>
-          </button>
-        </div>
-        <div className="card">
-          <img src="https://via.placeholder.com/300x200" alt="cover" />
-          <h6>Product Name</h6>
-          <p>R$ 15,00</p>
-          <div id="quantity">
-            <span>Sold: 20</span>
-            <span>Available: 5</span>
-          </div>
-          <button type="button">
-            <div className="icon">
-              <AiOutlineShopping size={19} />
-              <span>3</span>
-            </div>
-            <p>ADICIONAR AO CARRINHO</p>
-          </button>
-        </div>
-        <div className="card">
-          <img src="https://via.placeholder.com/300x200" alt="cover" />
-          <h6>Product Name</h6>
-          <p>R$ 15,00</p>
-          <div id="quantity">
-            <span>Sold: 20</span>
-            <span>Available: 5</span>
-          </div>
-          <button type="button">
-            <div className="icon">
-              <AiOutlineShopping size={19} />
-              <span>3</span>
-            </div>
-            <p>ADICIONAR AO CARRINHO</p>
-          </button>
-        </div>
-        <div className="card">
-          <img src="https://via.placeholder.com/300x200" alt="cover" />
-          <h6>Product Name</h6>
-          <p>R$ 15,00</p>
-          <div id="quantity">
-            <span>Sold: 20</span>
-            <span>Available: 5</span>
-          </div>
-          <button type="button">
-            <div className="icon">
-              <AiOutlineShopping size={19} />
-              <span>3</span>
-            </div>
-            <p>ADICIONAR AO CARRINHO</p>
-          </button>
-        </div>
-        <div className="card">
-          <img src="https://via.placeholder.com/300x200" alt="cover" />
-          <h6>Product Name</h6>
-          <p>R$ 15,00</p>
-          <div id="quantity">
-            <span>Sold: 20</span>
-            <span>Available: 5</span>
-          </div>
-          <button type="button">
-            <div className="icon">
-              <AiOutlineShopping size={19} />
-              <span>3</span>
-            </div>
-            <p>ADICIONAR AO CARRINHO</p>
-          </button>
-        </div>
-        <div className="card">
-          <img src="https://via.placeholder.com/300x200" alt="cover" />
-          <h6>Product Name</h6>
-          <p>R$ 15,00</p>
-          <div id="quantity">
-            <span>Sold: 20</span>
-            <span>Available: 5</span>
-          </div>
-          <button type="button">
-            <div className="icon">
-              <AiOutlineShopping size={19} />
-              <span>3</span>
-            </div>
-            <p>ADICIONAR AO CARRINHO</p>
-          </button>
-        </div>
+        ))}
       </Main>
     </>
   );
